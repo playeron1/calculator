3c04241a634932ef24c13e378e63e09d6381d93f
playeron1 <krishnshah2003@gmail.com>
2025-10-16 15:14:27 +0530

    completed my 1st HTML css JS project. mostly on my own. 40% on my own and rest through youtube but very used chatgpt and ai tools for finding bugs in my code and asking where I went wrong

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
-function intake_number(){
-    let num = document.getElementById("h1").innerHTML
-    let disp = num + this.innerHTML;
-}
+let string = "";
+let buttons = document.querySelectorAll('.button');
+
+Array.from(buttons).forEach((button) => {
+  button.addEventListener('click', (e) => {
+    if (e.target.innerHTML == '=') {
+      string = eval(string);
+      console.log(string);
+      document.querySelector('.output_disp').textContent = string;
+    } else if (e.target.innerHTML == "CE") {
+      string = "";
+      document.querySelector('.output_disp').textContent = string;
+    } else if (e.target.innerHTML == "*") {
+      string += "*";
+      document.querySelector('.output_disp').textContent = string;
+    } else {
+      string += e.target.innerHTML;
+      document.querySelector('.output_disp').textContent = string;
+    }
+  });
+});
